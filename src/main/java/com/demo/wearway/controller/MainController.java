77eaa71e47d2f0315bc96390a21116912a0147c9
lomoede <lomoede@naver.com>
2023-10-15 20:51:48 +0900

    frontend1

--- src/main/java/com/demo/wearway/controller/MainController.java
+++ src/main/java/com/demo/wearway/controller/MainController.java
@@ -1,4 +1,18 @@
 package com.demo.wearway.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/main")
 public class MainController {
+    @GetMapping("/hello")
+    public String hello() { return "Hello World!"; }
+    @GetMapping("/bye")
+    public String bye() { return "Bye World!"; }
+    @PostMapping("/getPerson")
+    public String getPerson(@RequestBody Map<String, String> map) {
+        return "Hello " + map.get("id");
+    }
 }
