7e688fbc633bd833a793f03ca7639e3f9866b924
lomoede <lomoede@naver.com>
2023-11-17 17:41:55 +0900

    frontend1

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import styled from "styled-components";
+// Pages
+import MainPage from './component/page/MainPage';
+
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+function App(props) {
+  return (
+      <BrowserRouter>
+        <MainTitleText>Wearway</MainTitleText>
+        <Routes>
+          <Route index element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
